1e2b218acc367b850a5ec8a5ee5708908b7eb70b
Darvesh Gorhe <darveshgorhe@gmail.com>
2023-09-13 21:03:28 +0000

    feat: robust python version checks and OS-specific conditionals

--- setup.sh
+++ setup.sh
@@ -1,24 +1,53 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-cp ./.vimrc ~/.vimrc
-vim ~/.vimrc -c "PluginInstall" -c "qa"
-
 #!/bin/bash
 
-# Check Python version
-python_version=$(/usr/bin/python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+# Detect Operating System
+case "$(uname -s)" in
+    Linux*)     os=Linux;;
+    Darwin*)    os=Mac;;
+    CYGWIN*)    os=Windows;;
+    MINGW*)     os=Windows;;
+    *)
+        echo "Unknown OS"
+        exit 1
+        ;;
+esac
 
-# Split the version into an array
-IFS='.' read -ra version_parts <<< "$python_version"
-
-# Check major and minor version
-major_version=${version_parts[0]}
-minor_version=${version_parts[1]}
-
-if [ "$major_version" -ge 3 ] && [ "$minor_version" -ge 8 ]; then
-    cd ~/.vim/bundle/YouCompleteMe
-    /usr/bin/python3 install.py --all
-else
-    echo "Python version 3.8 or higher is required to install youcompleteme (don't use conda python path)"
+# OS-specific Logic
+if [ "$os" == "Linux" ]; then
+    echo "Linux detected"
+    # Your logic here
 fi
+
+if [ "$os" == "Mac" ]; then
+    echo "Mac detected"
+    # Your logic here
+fi
+
+if [ "$os" == "Windows" ]; then
+    echo "Windows detected"
+    # Your logic here
+fi
+
+# Check Python version
+python_path=""
+for py_version in $(ls /usr/bin | grep -E 'python3(\.[0-9]+)?$'); do
+    version_string=$(/usr/bin/$py_version -c 'import sys; print(".".join(map(str, sys.version_info[:3])))' 2>/dev/null)
+    IFS='.' read -ra version_parts <<< "$version_string"
+    major_version=${version_parts[0]}
+    minor_version=${version_parts[1]}
+  
+    if [ "$major_version" -eq 3 ] && [ "$minor_version" -ge 8 ]; then
+        python_path="/usr/bin/$py_version"
+        break
+    fi
+done
+
+if [ -z "$python_path" ]; then
+    echo "No suitable Python version found in /usr/bin"
+    exit 1
+fi
+
+# Installation logic
+cd ~/.vim/bundle/YouCompleteMe
+$python_path install.py --all
 
