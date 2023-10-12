6b726fa7ac33bb24d7300d7b64696494022e797a
Darvesh Gorhe <darveshgorhe@gmail.com>
2023-10-12 13:52:56 +0000

    changing author

--- setup.sh
+++ setup.sh
@@ -24,6 +24,24 @@ if [ "$os" == "Linux" ]; then
 
     # Install all the Vundle plugins and then quit all windows in vim
     vim -c "PluginInstall" -c "qa"
+
+    # Install pre-requisites for youcompleteme on Ubuntu
+    sudo apt install build-essential cmake vim-nox python3-dev -y
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_current.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm -y
+
+    # Install miniconda
+    mkdir -p ~/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
+    ~/miniconda3/bin/conda init bash
+
+    # Install Miniforge3
+    wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+    bash Miniforge3-$(uname)-$(uname -m).sh
 fi
 
 if [ "$os" == "Mac" ]; then
@@ -41,7 +59,7 @@ fi
 
 if [ "$os" == "Windows" ]; then
     echo "Windows detected"
-    echo "I don't use Windows computers so idk how to install vim the right way"
+    echo "I don't use Windows computers so idk how to install vim the right way :/"
 fi
 
 # Check Python version
